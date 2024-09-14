7db68f43168de4e57e1b27b926c46e28e4fabd64
jhondiaz18 <jhon_0621@hotmail.com>
2024-09-14 17:42:45 -0500

    Adición componente barra de navegación

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import Login from './Components/Security/Login';
 import Registro from "./Components/Security/Registro";
+import MenuAppBar from "./Components/Navegation/MenuAppBar";
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
+      <MenuAppBar />
       <Registro />
     </div>
   );
