ed08650eccdd152787e3ae5924c3da6b4e5720bc
jhondiaz18 <jhon_0621@hotmail.com>
2024-09-27 12:56:18 -0500

    cambio boton de cambio de idioma

--- src/Components/Navegation/NavBar.jsx
+++ src/Components/Navegation/NavBar.jsx
@@ -15,12 +15,14 @@ import InfoIcon from "@mui/icons-material/Info";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import LanguageIcon from "@mui/icons-material/Language";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 const NavBar = ({ toggleTheme, mode }) => {
   const { t, i18n } = useTranslation();
   const [anchorEl, setAnchorEl] = useState(null);
+  const [languageAnchorEl, setLanguageAnchorEl] = useState(null);
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -30,8 +32,17 @@ const NavBar = ({ toggleTheme, mode }) => {
     setAnchorEl(null);
   };
 
+  const handleLanguageMenu = (event) => {
+    setLanguageAnchorEl(event.currentTarget);
+  };
+
+  const handleLanguageClose = () => {
+    setLanguageAnchorEl(null);
+  };
+
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
+    handleLanguageClose();
   };
 
   return (
@@ -83,12 +94,21 @@ const NavBar = ({ toggleTheme, mode }) => {
           <IconButton color="inherit" onClick={toggleTheme}>
             {mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
           </IconButton>
-          <Button color="inherit" onClick={() => changeLanguage("en")}>
-            ES
-          </Button>
-          <Button color="inherit" onClick={() => changeLanguage("es")}>
-            EN
-          </Button>
+          <IconButton color="inherit" onClick={handleLanguageMenu}>
+            <LanguageIcon />
+          </IconButton>
+          <Menu
+            anchorEl={languageAnchorEl}
+            open={Boolean(languageAnchorEl)}
+            onClose={handleLanguageClose}
+          >
+            <MenuItem onClick={() => changeLanguage("en")}>
+              <ListItemText primary="Español" />
+            </MenuItem>
+            <MenuItem onClick={() => changeLanguage("es")}>
+              <ListItemText primary="English" />
+            </MenuItem>
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>
